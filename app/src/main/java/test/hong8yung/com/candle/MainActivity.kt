77e779fd8c63560f14e296187c2ae69743ab158e
turtlina <turtlinahh@gmail.com>
2019-05-26 08:04:04 +0900

    init: new project & for test

---
+++ app/src/main/java/test/hong8yung/com/candle/MainActivity.kt
@@ -0,0 +1,12 @@
+package test.hong8yung.com.candle
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
